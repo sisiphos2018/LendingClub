98c48308916b6ac0d6432d1f1c621b5060a21a53
ThomasDing2016 <td2488@columbia.edu>
2016-12-07 02:17:59 -0500

    Delete EDA.R

--- LoanDefaultAnalysis/EDA.R
+++
@@ -1,8 +0,0 @@
-library(corrplot)
-LoanNoNANum=Filter(is.numeric,LoanNoNA)
-M <- cor(LoanNoNANum)
-corrplot(M, method="circle",type="upper")
-corrplot.mixed(M)
-
-#Many vars are strongly correlated
-mode(loan_raw$month)
